2fa02bddfeb8680c8fbf66fbf0abc9767dbad56a
pedrohmtoledo <toledopedro.hmt@gmail.com>
2024-10-14 21:22:10 +0100

    Fix getoperandsandoperator function

--- javascript.js
+++ javascript.js
@@ -1,22 +1,54 @@
 
-// create variable to store the button pressed. show the button pressed on the display
-let display = document.querySelector("#display");
+// create a array to store the buttons pressed. show the buttons pressed on the display.
+const display = document.querySelector("#display");
+
 let value = [];
-const button = document.querySelectorAll(".number");
+const operandsArray = ["add", "subtract", "multiply", "divide"]
+let firstOperator = null;
+let secondOperator = null;
+let operand = null;
+let result;
+let indexOfOperand;
+
+const button = document.querySelectorAll("button");
 button.forEach((button) => {
     button.addEventListener("click", () => {
         value.push(button.id)
         let number = document.createTextNode(button.id);
         display.appendChild(number);  
-        console.log(value)    
+        getOperatorsAndOperand(value);    
     })
 })
 
 
-let firstOperator = 33;
-let secondtOperator = 11;
-let operand = "divide";
+// function to breakdown the array clicked in the calculator and calculate using operate function.
 
+function getOperatorsAndOperand(array){
+    
+    for (let i = 0; i < array.length; i++) {
+
+        if(operandsArray.includes(array[i]) && array.indexOf(array[i]) === 0){
+            firstOperator = 0;
+            operand = array[i];
+            indexOfOperand = array.indexOf(operand);
+        
+        }
+        else if(operandsArray.includes(array[i]) && array.indexOf(array[i]) > 0 && firstOperator === null){
+            operand = array[i];
+            firstOperator = parseInt(array.slice(0,i).join(""));
+            indexOfOperand = array.indexOf(operand); 
+            console.log(indexOfOperand)
+             
+        }
+        else if(operandsArray.includes(array[i]) && array.indexOf(array[i]) > 0 && firstOperator != null){
+            secondOperator = parseInt(array.slice((indexOfOperand + 1), (array.length)).join(""))
+            result = operate(firstOperator,secondOperator,operand);
+            console.log(result);
+              
+        }       
+            
+    }
+}
 
 // Function to exec a operation with 2 numbers
 function operate(first, second, operand){
@@ -57,15 +89,6 @@ function divide(a, b){
 
 
 
-
-
-
-// const a = 5
-// const b = 9
-
-// console.log(add(a,b))
-// console.log(subtract(a,b))
-// console.log(divide(a,b))
 
 
 
