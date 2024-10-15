c335a2bc2a6ddc6901eb9ef91694dc206b678581
pedrohmtoledo <toledopedro.hmt@gmail.com>
2024-10-15 19:28:21 +0100

    Code the calculator
    Implementing the calculator doing the calculations
    when a second operand is pressed

--- javascript.js
+++ javascript.js
@@ -6,49 +6,97 @@ let value = [];
 const operandsArray = ["add", "subtract", "multiply", "divide"]
 let firstOperator = null;
 let secondOperator = null;
-let operand = null;
+let operand = [];
 let result;
-let indexOfOperand;
+// let indexOfOperand;
+// let operatorCounter = 0;
 
 const button = document.querySelectorAll("button");
 button.forEach((button) => {
     button.addEventListener("click", () => {
-        value.push(button.id)
-        let number = document.createTextNode(button.id);
-        display.appendChild(number);  
-        getOperatorsAndOperand(value);    
+        if (button.className === "number"){ 
+            value.push(button.id);
+            // let numbersTyped = parseInt(value.join(""))
+            // document.getElementById("display").innerHTML = ""
+            // textDIsplay = document.createTextNode(numbersTyped);
+            // display.appendChild(textDIsplay);
+            // getOperatorsAndOperand(value); 
+        }
+        else if (button.className === "operand" && typeof result != "number"){ 
+            operand.push(button.id);
+            console.log(result)
+            getOperatorsAndOperand(value); 
+            document.getElementById("display").innerHTML = "";
+        }
+        else if (button.id === "clear"){
+            firstOperator = null;
+            secondOperator = null;
+            operand = [];
+            value = [];
+            document.getElementById("display").innerHTML = "";
+        }
+        else if(typeof result === "number" && button.className === "operand"){
+            document.getElementById("display").innerHTML = result;
+            result = null;
+        }
+       
+        console.log(value)
+        console.log(operand.length)
     })
 })
 
 
 // function to breakdown the array clicked in the calculator and calculate using operate function.
+// fix the function below
 
 function getOperatorsAndOperand(array){
-    
-    for (let i = 0; i < array.length; i++) {
-
-        if(operandsArray.includes(array[i]) && array.indexOf(array[i]) === 0){
-            firstOperator = 0;
-            operand = array[i];
-            indexOfOperand = array.indexOf(operand);
+            
+    if(operand.length > 0 && array.length === 0){
+        operand.shift()
+        console.log(operand);
+        console.log("primeiro if")
+            
         
-        }
-        else if(operandsArray.includes(array[i]) && array.indexOf(array[i]) > 0 && firstOperator === null){
-            operand = array[i];
-            firstOperator = parseInt(array.slice(0,i).join(""));
-            indexOfOperand = array.indexOf(operand); 
-            console.log(indexOfOperand)
-             
-        }
-        else if(operandsArray.includes(array[i]) && array.indexOf(array[i]) > 0 && firstOperator != null){
-            secondOperator = parseInt(array.slice((indexOfOperand + 1), (array.length)).join(""))
-            result = operate(firstOperator,secondOperator,operand);
-            console.log(result);
+    }
+    else if(operand.length > 0 && array.length > 0 && typeof firstOperator != "number"){
+        firstOperator = parseInt(array.slice().join(""));
+        array.splice(0, array.length);
+        operand.slice(0, operand.length)
+        console.log("test")
+        console.log(firstOperator)
+        console.log(array)
+                         
+    }
+    else if(operand.length === 2 && typeof(firstOperator) === "number"){
+        secondOperator = parseInt(array.slice().join(""))
+        result = operate(firstOperator,secondOperator,operand[0]);
+        operand.shift()
+        console.log(`this is first ${firstOperator}`)
+        console.log(`this is second ${secondOperator}`);
+        console.log(result);
+        console.log(array);
+        array.splice(0, array.length);
+        firstOperator = result;
+        console.log("ultimo if")
+
+
+            // console.log(array);
+            // array.splice(0,array.length, result);
+            // array.push(operand);
+            // firstOperator = result;
+            // secondOperator = null;
+            // console.log(value);
+            // console.log(firstOperator);
+            // console.log("test");
+            
+            // indexOfOperand = array.indexOf(array[i])
+            // console.log(firstOperator)
+            // console.log(indexOfOperand)   
               
         }       
             
     }
-}
+
 
 // Function to exec a operation with 2 numbers
 function operate(first, second, operand){
@@ -82,7 +130,7 @@ function subtract(a ,b){
 function multiply(a, b){
     return a * b;
 }
-
+ 
 function divide(a, b){
     return a / b;
 }
